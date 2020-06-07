a9531585aecc5905ff2398d0f7f2bebabb4abf39
Shubhankar Gambhir <Shubhankargambhir013@gmail.com>
2020-06-07 18:05:46 +0530

    Update create.js

--- Write_Transaction/create.js
+++ Write_Transaction/create.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 const transaction = require('./Create_Transaction_Classes/Create_Transaction');
-
-var Transaction = new transaction();
+var Key = fs.readFileSync('../PrivateKeys/p1_private.pem')
+var Transaction = new transaction(Key);
 
 file = '../Transactions/' + Transaction.ID + '.dat';
 fs.createWriteStream(file);
