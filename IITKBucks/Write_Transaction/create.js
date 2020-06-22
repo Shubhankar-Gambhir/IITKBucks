58917dacf1ab9f557072d9db742af143e5e1a5a2
Shubhankar Gambhir <shubhankar@shubhankar.org>
2020-06-22 23:42:57 +0530

    Remove duplicated directory

--- IITKBucks/Write_Transaction/create.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const transaction = require('./Create_Transaction_Classes/Create_Transaction');
-
-var Transaction = new transaction();
-
-file = '../Transactions/' + Transaction.ID + '.dat';
-fs.createWriteStream(file);
-fs.writeFileSync(file,Transaction.Buf);
