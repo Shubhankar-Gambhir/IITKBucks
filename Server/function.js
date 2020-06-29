0352bd9f08adc31b1c4b3be6ed9aa3a375465fd8
Shubhankar Gambhir <Shubhankargambhir013@gmail.com>
2020-06-29 11:31:44 +0530

    Add files via upload

--- Server/function.js
+++ Server/function.js
@@ -1,37 +1,39 @@
 const axios = require('axios');
 const fs = require('fs');
 const crypto = require('crypto');
+
 const Transaction = require('../Write_Transaction/Create_Transaction_Classes/Create_Transaction.js');
 const Block = require('../Block/Read_Block_Classes/Block.js');
 
-async function get_Peers (peers_arr,peers_lim,My_url){
-    let peers = [];
-    for(let url of peers_arr){      
-        await axios.post(url + '/newPeer',{"url": My_url},{headers: {'Content-Type': 'application/json'}})
-        .then(function (res) {
-            if(peers.length >= peers_lim){return peers};
-            if(res.status == 200){peers.push(url)}; 
-        })
-        .catch(function(error){console.error(error)});
+async function get_Peers (known_nodes,used_urls,peers_lim,My_url){
+    var peers = [];
+    for(let url of known_nodes){      
+        await axios.post(url + '/newPeer',{"url": My_url},{headers: {'Content-Type': 'application/json'}})            // Get peers from peers_Arr
+        .then(function (res) {if(peers.length < peers_lim){peers.push(url)}})                                         // Add them to the list of peers
+        .catch(function(error){if(error.response.status!=500) console.error(error)})
+        used_urls.push(url);                                                                                          // Adds the url to the Array used_urls
     }
-    var new_peers_lim = peers_lim - peers.length;
+    
     if(peers.length < peers_lim){       
         var Potential_Peers = [];
-        for(let url of peers_arr){
-            await axios.get(url +'/getPeers')                                                   // Get potential peers from peers
-            .then(function(res){Potential_Peers = [...Potential_Peers,...res.data.peers]})    // Add them to the list of potential peers
-            .catch(function(error){console.error(error)});
+        
+        for(let url of known_nodes){
+            await axios.get(url +'/getPeers')                                                                          // Get potential peers from peers
+            .then(function(res){Potential_Peers = [...Potential_Peers,...res.data.peers]})                             // Add them to the list of potential peers
+            .catch(function(error){console.error(error.response)});
         }                                                        
-        Potential_Peers = Potential_Peers.filter(function(value, index, self) {return self.indexOf(value) === index;}) // remove repeated elements 
-        Potential_Peers = Potential_Peers.filter(function(item) {return !peers.includes(item);})                       // remove elements of peer
+        
+        Potential_Peers = Potential_Peers.filter(function(value, index, self) {return self.indexOf(value) === index;}) // filter for unique elements 
+        Potential_Peers = Potential_Peers.filter(function(item) {return !used_urls.includes(item);})                   // remove already uesd elements 
         Potential_Peers = Potential_Peers.filter(function(a) {return a != My_url;})                                    // remove self
-        await get_Peers(Potential_Peers,new_peers_lim,My_url)     // Get peers from potential peers
-        .then(function (Arr) {                                    // Add them to the list of peers
-            peers = [...peers,...Arr];                            
-            return peers;
-        })
-        .catch(function(error){console.error(error);})                         
+       
+        if(Potential_Peers.length){
+            await get_Peers(Potential_Peers,used_urls,peers_lim-peers.length,My_url)                                   // Get peers from potential peers
+            .then(function (Arr) {peers = [...peers,...Arr]})                                                          // Add them to the list of peers   
+            .catch(function(error){console.error(error);})   
+        }                          
     }
+
     return peers
 }
 
@@ -39,47 +41,60 @@ async function get_Blocks(url){
     var status = 200;
     var index = 0;
     while(status != 404){
-        await axios.get(url+'/add/'+ index.toString()).then(function(res){
-            console.log(index);
-            status = res.status;
+        await axios.get(url+'/add/'+ index,{responseType: 'arraybuffer'})
+        .then(function(res){                                                                                            // Asks a peer for Block
             var New_Block = new Block(res.data);
-            New_Block.Update_Unused_Output();                           //Updates the Unused_Outputs.txt
-            fs.writeFileSync('../Blocks/Block'+index.toString()+'.dat',res.data);
-            index += 1;
-        })
+            New_Block.Update_Unused_Output();                                                                           // Updates the Unused_Outputs.txt
+            fs.writeFileSync('../Blocks/Block'+index.toString()+'.dat',res.data);                                       // Stores Block in memory
+            index += 1; })
+        .catch(function(error){console.error(error);})                                                        // Updates the status if it is 404
+                                 
     }
+    return index;
 }
 
 async function get_Pending_Transaction(url){
     var Pending_Transactions = [];
-    await axios.get(url + '/getPendingTransactions').then(function(res){
+
+    await axios.get(url + '/getPendingTransactions')                                                                    // Asks a peer for Pending_Transactions
+    .then(function(res){                                                                                                // Adds them to ao our Array
         for(let Txn of res.data){
-        Pending_Transactions.push(new Transaction(Txn.inputs,Txn.outputs,0));
-        }
-    })
+            Pending_Transactions.push(new Transaction(Txn.inputs,Txn.outputs,0));
+        }})
+    .catch(function(error){console.error(error);})                         
+
     return Pending_Transactions;
 }
 
-module.exports.Initialize = async function (peers_arr,peers_lim,My_url) {
-    var peer = [],Pending_Transactions = [];
+module.exports.Initialize = async function (peers_arr,peers_lim,My_url) {                                                 // All the above functions in 1 function 
+    var Peer = [],Pending_Transactions = [],index = 0;
 
-    await get_Peers(peers_arr,peers_lim,My_url)
-    .then(function list(peers_list){peer = peers_list})
+    await get_Peers(peers_arr,Peer,peers_lim,My_url)                                                                      // Sets the Peers
+    .then(function list(peers_list){Peer = peers_list})
     .catch(function(error){console.error(error)});
 
-    await get_Pending_Transaction(peer[0])
+    await get_Pending_Transaction(Peer[0])                                                                                // Sets Pending_Transactions
     .then(function(Transactions_list){Pending_Transactions = Transactions_list})
     .catch(function(error){console.error(error)});
 
-    //await get_Blocks(peer[0]);
-    
-    return [peer,Pending_Transactions];
+    await get_Blocks(Peer[0])                                                                                             // Stores The Blocks in storage
+    .then(function (Index) { index = Index})                                                                              // and gives the current Index
+    .catch(function(error){console.error(error)});
+
+    return [Peer,Pending_Transactions,index];
 }
 
-module.exports.get_Hash = function(index,folder){
-    if (index){
-        var Byte = fs.readFileSync(folder+'/Block'+index+'.dat');
-        return crypto.createHash('SHA256').update(Byte).digest('hex');
+module.exports.get_Hash = function(index){
+    if (index + 1){
+        var path =  __filename.split('IITKBucks')[0].toString() + 'IITKBucks/Blocks/'
+        var Byte = Buffer.from(fs.readFileSync(path+'Block'+index+'.dat'));
+        return crypto.createHash('SHA256').update(Byte.slice(0,116)).digest('hex');
     }
     else {return 0000000000000000000000000000000000000000000000000000000000000000;}
+}
+
+module.exports.send_to_peers = function (peers,data,api) {
+    for(let url of peers){
+        axios.post(url+api,data)
+    }
 }
--- Server/server.js
+++ Server/server.js
@@ -5,34 +5,33 @@ const fs = require('fs');
 const Transaction = require('../Write_Transaction/Create_Transaction_Classes/Create_Transaction.js');
 const Block = require('../Block/Read_Block_Classes/Block_Body');
 const Create_Block = require('../Block/Create_Block_Classes/Block')
-const initialization = require('./function');
+const funcs = require('./function');
 
-let known_nodes = ['http://8c0090bc.ngrok.io' ,'http://52d3004ed431.ngrok.io'];//temporary urls
-var Index = 0
-var Index = 0
-var Pending_Transactions =[];
-var peers =[];
+let known_nodes = ['http://localhost:8000'];//temporary urls
+var Index = 0,Pending_Transactions =[],peers =[];
 
-// initialization.Initialize(known_nodes,1,'http://localhost:'+port)
-// .then(function (Arr) {
-//     peers = Arr[0];
-//     Pending_Transactions = Arr[1];
-// })
-// .catch(function(error){console.error(error)});
+funcs.Initialize(known_nodes,1,'http://localhost:8080')
+.then(function (Arr) {
+    peers = Arr[0];
+    Pending_Transactions = Arr[1];
+    Index = Arr[2];
+})
+.catch(function(error){console.error(error)});
 
 const app = express();
 
 app.use('getBlock',express.static('../Blocks',{ root : __dirname}));
 app.use (bodyParser.urlencoded({extended : true}));
 app.use (bodyParser.json());
-var Num_Blocks = 0;
-
 
 app.get('/add/:Block_Index',function(req,res){
     var File_name = '../Blocks/Block' + req.params.Block_Index.toString()+'.dat' ;
-    var Byte = fs.readFileSync(File_name);
-    res.setHeader("Content-Type", "application/octet-stream");
-    res.send(Byte);
+    if(fs.existsSync(File_name)){
+        var Byte = fs.readFileSync(File_name);
+        res.setHeader("Content-Type", "application/octet-stream")
+        res.send(Byte);
+    }
+    else{res.status(404).send('Block Not Found');}
 })
 
 app.get('/getPendingTransactions',function(req,res){
@@ -47,52 +46,37 @@ app.get('/getPeers',function(req,res){
 });
 
 app.post('/newPeer',function(req,res){
-    var Is_peer_new = true;
-    if(peers.length <= 2) {                                 // Check if their is space for new peer
-        for(let peer of peers) {
-            if(peer == req.body.url) {                      // Check if the peers is new or repeated
-                res.status(200).send('Peer already exist');
-                Is_peer_new = false;
-            }
-        }
-        if(Is_peer_new){                                    // If peer is new it is added to the Array of peers
-            console.log("new Peer: " + req.body.url);
+    
+    if(peers.includes(req.body.url)) {res.status(200).send('Peer already exist');}
+    else {
+        if(peers.length <= 2){                                      // Check if their is space for new peer
+            console.log("new Peer: " + req.body.url);               // If peer is new & there is enough space peer is added to the Array of peers
             peers.push(req.body.url);
             res.status(200).send('Peer Added');
         }
+        else res.status(500).send('Peer Limit reached!');     
     }
-    else res.status(500).send('Peer Limit reached!');
 })
 
 app.post('/newBlock',function(req,res){
     Num_Blocks++;
-    var New_Block = new Block(res.data);                                                 // Processing Block
+    var New_Block = new Block(res.data);                                                 // Processing Block:
     New_Block.Update_Unused_Output();                                                    // 1) Updates Unused_Oututs.txt
     Pending_Transactions = New_Block.Update_Pending_Transactions(Pending_Transactions);  // 2) Updates Pending_Transactions
     fs.writeFileSync('../Blocks/' + New_Block.Index , req.body);                         // 3) Stores the New Block
+    funcs.send_to_peers(peers,req.body,'/newBlock');
     res.send('Block Added');
 })
 
 app.post('/newTransaction',function(req,res){
     var New_Transaction = new Transaction(req.body.inputs,req.body.outputs,0)
-    var Is_Transaction_New = true;
-    for(let Txn of Pending_Transactions) {                    // Check if Transaction is new or repeated
-        if(Txn.ID == New_Transaction.ID){
-            res.send('Transaction Already Exist');
-            Is_Transaction_New = false;
-        } 
-    }
-    if(Is_Transaction_New) {                                  // Adds Transaction to Array of Pending_Transactions
-        Pending_Transactions.push(New_Transaction);
+
+    if(Pending_Transactions.includes(New_Transaction)) {res.send('Transaction Already Exist');}
+    else{
+        Pending_Transactions.push(New_Transaction);                                       // Adds Transaction to Array of Pending_Transactions
+        funcs.send_to_peers(peers,req.body,'/newTransaction')                             // Sends it to peers
         res.send('Transaction Added');
     }
-})
-app.post('/CreateBlock',function (req,res) {
-    Index++
-    var block = new Create_Block(Index,'0000f00000000000000000000000000000000000000000000000000000000000',Pending_Transactions,'../Blocks');
-    console.log(block.Block_Buf)
-    fs.writeFileSync('../Blocks/Block'+ Index+'.dat',block.Block_Buf);
-    res.send(block);
 })
 
 
